70a4a83019a13856bd518cb39607d7bad10242f2
DESKTOP-PH7JJ39\tj-bu-04 <pass2149@gmail.com>
2023-01-06 13:19:36 +0900

    test 버전추가

--- main-section1.php
+++ main-section1.php
@@ -2,37 +2,34 @@
 <script src="./js/index/layout.js"></script>
 <!-- 본문시작 -->
 
-<div class="text-box">
-  <h2 class="section-title">
-    서야궁 소개
-  </h2><!-- 제목 -->
-  <p class="section-desc">
-    과거와 현재의 운치 있는 만남
-  </p><!--부제목 -->
-</div><!-- text-box -->
+<div class="sticky-container">
+  <div class="sticky">
+    <div class="text-box">
+      <h2 class="section-title">
+        서야궁 소개
+      </h2><!-- 제목 -->
+      <p class="section-desc">
+        과거와 현재의 운치 있는 만남
+      </p><!--부제목 -->
+    </div><!-- text-box -->
 
-<div class="slider-section1">
-  <div class="sticky-container">
-    <div class="sticky">
-      <div class="sticky-inner">
-
+    <div class="sticky-inner">
+      <div class="sticky-inner-container">
         <section class="palace1">
-          <div class="obj1">
-            <p class="palace-h ebs">경복궁</p>
             <div class="palace-img">
               <figcaption class="img-box">
                 <img src="" alt="">궁사진
               </figcaption><!-- 경복궁사진 -->
-
               <div class="palacae-text-all">
+              <p class="palace-h ebs">경복궁</p>
                 <p class="palace-name">덕수궁</p>
                 <p class="palace-name2">부제</p>
                 <p class="palacae-text">궁텍스트</p>
-                <p class="palace-zoom">자세히보기</p>
+                <a class="palace-zoom" href="#">자세히보기</a>
               </div><!-- 경복궁텍스트 -->
             </div><!-- pal경복사진전체 -->
-          </div><!-- obj1 -->
         </section><!-- palace1 경복궁 -->
+
         <section class="palace2">
           palace2
         </section><!-- palace2 덕수궁 -->
@@ -42,7 +39,7 @@
         <section class="palace4">
           palace4
         </section><!-- palace4 창덕궁 -->
-      </div><!--sticky-inner  -->
-    </div><!--sticky  -->
-  </div><!--sticky-container  -->
-</div><!-- slider-section1 경복궁 -->
+      </div><!--sticky-inner-container  -->
+    </div><!--sticky-inner  -->
+  </div><!--sticky  -->
+</div><!--sticky-container  -->
