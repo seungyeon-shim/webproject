0905689c7c3f7ee96c399f8e48625c31dbe5e555
sowon <thwls9930@gmail.com>
2023-01-08 17:59:09 +0900

    sub-history 오브젝트 추가 및 구조 설계

--- sub-kbg/sub-history-kbg.php
+++ sub-kbg/sub-history-kbg.php
@@ -17,15 +17,37 @@
     <img class="flower flower2" src="./img/sub/sub-deco/Flower1.png" alt="">
   </figure>
 
-  <div class="content">
-    <ul class="yearly-tap">
-      <li>1300</li>
-      <li>1400</li>
-      <li>1500</li>
-      <li>1600</li>
-      <li>1700</li>
-      <li>1800</li>
-    </ul>
-  </div>
+  <div class="content"><!-- 전체 -->
+    <div class="c1"><!-- 상단바  -->
+      <img src="../img/sub/sub-history/kbg/history-bar.png" alt="">
+      <div class="bar-textbox">
+        <p>1300 ~ 1399</p>
+        <p>1300 <b class=".point-sans">年</b></p>
+      </div>
+    </div><!-- 상단 바  -->
+    <div class="c2">
+      <div class="c2-textbox">
+      <i> > </i>
+      <h2> 제목</h2>
+      <p> 역사 어쩌구</p>
+      </div>
+      <figure>
+        <img src="" alt="">
+        <img src="" alt="">
+      </figure>
+    </div>
+    <div><!-- nav -->
+      <div><!-- 윗줄 -->
+        <button>1300</button>
+        <button>1400</button>
+        <button>1500</button>
+      </div><!-- 윗줄 -->
+      <div><!-- 아래줄 -->
+        <button>1800</button>
+        <button>1900</button>
+        <button>2000</button>
+      </div><!-- 아래줄 -->
+    </div><!-- nav -->
+  </div><!-- content -->
 
 </section>
