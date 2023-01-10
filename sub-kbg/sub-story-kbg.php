671fd5bcf40529037ecd212c9f27798a4b1f328c
DESKTOP-44C1DQM\tj-bu <nini0221@naver.com>
2023-01-10 18:54:43 +0900

    주은 23.01.10 학원에서 수정

--- sub-kbg/sub-story-kbg.php
+++ sub-kbg/sub-story-kbg.php
@@ -1,33 +1,44 @@
 <link rel="stylesheet" href="./css/sub-kbg/sub-story.css">
 
-<section class="sub-content sub-story sub-story-kbg">
-    
-    <div class="sub-title-textbox">
+<section class="sub-content sub-story sub-story-kbg sub-content-center">
+
+    <img class="back-pattern" src="./img/sub/sub-deco/back-pattern.png" alt="">
+    <img class="story-left" src="./img/sub/sub-deco/story-left.png" alt="">
+    <img class="story-right" src="./img/sub/sub-deco/story-right.png" alt="">
+    <img class="wave" src="./img/sub/sub-deco/wave.png" alt="">
+
+    <div class="sub-title-wrap sub-title-wrap-center">
         <p class="sub-desc">조선의 문화와 역사가 깃든</p>
         <h2 class="sub-title">경복궁 이야기</h2>
     </div>
 
-    <figure class="img-box">
-        <img src="../img/sub/sub-deco/back-pattern.png" alt="">
-        <img src="../img/sub/sub-deco/story-left.png" alt="">
-        <img src="../img/sub/sub-deco/story-right.png" alt="">
-        <img src="../img/sub/sub-deco/folding-screen.png" alt="">
-        <img src="../img/sub/sub-deco/wave.png" alt="">
-    </figure>
 
     <div class="sub-middle-title-box">
+        
         <div class="sub-middle-title sub-middle-title1">
             <p class="sub-first-desc">명성황후 시해</p>
             <p class="sub-second-desc">향원정 북쪽에 있는 건청궁 <br> 곤녕합에서 일본인들에게 시해</p>
-        </div>
+            <img src="./img/sub/sub-story/kbg/original/1.png" alt="">
+        </div><!-- sub-middle-title1 -->
+
         <div class="sub-middle-title sub-middle-title2">
             <p class="sub-first-desc">오래 머물렀던 왕</p>
             <p class="sub-second-desc">세종대왕은 경복궁에서 오래 머무르면서 <br> 한글을 창제 후 과학기술 발전 도모</p>
-        </div>
+            <img src="./img/sub/sub-story/kbg/original/2.png" alt="">
+        </div><!-- sub-middle-title2 -->
+
         <div class="sub-middle-title sub-middle-title3">
             <p class="sub-first-desc">경회루 연못의 용</p>
             <p class="sub-second-desc">경회루 연못에서 청동으로 만든 <br> 용(龍) 두 마리가 발견</p>
-        </div>
-    </div>
+            <img src="./img/sub/sub-story/kbg/original/3.png" alt="">
+        </div><!-- sub-middle-title3 -->
+
+    </div><!-- sub-middle-title-box -->
+
     
+    
+    
+
+
+
 </section>
