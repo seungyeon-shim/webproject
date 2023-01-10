fa6f283ec5e1934e92ebedd2b170311db2f437fa
DESKTOP-M3T1IT9\tj-bu <ehgns0908@naver.com>
2023-01-10 18:30:19 +0900

    김진형

--- sub-kbg/sub-location-kbg.php
+++ sub-kbg/sub-location-kbg.php
@@ -3,12 +3,16 @@
   <div class="sub-title-wrap  sub-title-wrap-center">
     <h2 class="sub-title">경복궁 오시는길</h2>
     <p class="sub-desc">
-      평화통일정책의 수립에 관한 대통령의 자문에 응하기 위하여 민주평화통일자문회의를 둘 수 있다. 
-      학교교육 및 평생교육을 포함한 교육제도와 그 운영, 교육재정 및 교원의 지위에 관한 기본적인 사항은 법률로 정한다.
+      경복궁으로 오세요
     </p>
   </div><!-- sub-title-wrap -->
-  <div>
-    본문내용
+  <div class="location-wrap">
+    <img class="grap grap-land grap-left" src="./img/sub/sub-location/scroll-paper-grip.png" alt="">
+    <img class="grap grap-land grap-right" src="./img/sub/sub-location/scroll-paper-grip.png" alt="">
+    <img class="grap grap-port grap-top" src="./img/sub/sub-location/scroll-paper-grip-v.png" alt="">
+    <img class="grap grap-port grap-bottom" src="./img/sub/sub-location/scroll-paper-grip-v.png" alt="">
+    <img class="scr-paper scr-paper-land" src="./img/sub/sub-location/scroll-paper.png" alt="">
+    <img class="scr-paper scr-paper-port" src="./img/sub/sub-location/scroll-paper-v.png" alt="">
   </div>
   
 </section>
