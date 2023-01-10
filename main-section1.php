e8f875f306631e5c9756bf851cf9ea8ed9d460aa
DESKTOP-PH7JJ39\tj-bu-04 <pass2149@gmail.com>
2023-01-10 18:51:57 +0900

    main-section1 01.10일 수정 최준혁

--- main-section1.php
+++ main-section1.php
@@ -16,17 +16,23 @@
     <div class="sticky-inner">
       <div class="sticky-inner-container">
         <section class="palace palace1">
+        <div class="palace-name2 point-serif">景<br>福<br>宮</div>
           <figure>
             <img class="cloud" src="./img/main/section1-about/kbg/kbg-cloud.png" alt="">
             <img class="kbg" src="./img/main/section1-about/kbg/kbg-1.png" alt=""><!-- img -->
           </figure><!-- figure -->
           <div class="palacae-text-all">
+            <p class="palace-name">조선왕조 제일의 법궁</p>
             <p class="palace-h ebs">경복궁</p>
-            <p class="palace-name">덕수궁</p>
-            <p class="palace-name2">부제</p>
-            <p class="palacae-text">궁텍스트</p>
+            <p class="palacae-text">
+              조선건국과 함께 지어진 궁궐<br>
+              궁의 유례는 정도전의 시경의 한구절에서<br>
+              ‘큰복’을 의미하는 경복을 따왔습니다.<br>
+              수도 서울의 중심이고 조선의 으뜸 궁궐인<br>
+              경복궁에서 격조 높고 품위 있는 왕실 문화의 <br>
+              진수를 느껴보시기 바랍니다.</p>
             <a class="palace-zoom" href="#">
-              <p>자세히보기</p>
+              <p>자세히보기 <i class="fa-solid fa-arrow-right"></i></p>
             </a><!-- palace-zoom -->
           </div><!-- 경복궁텍스트 -->
         </section><!-- palace1 경복궁 -->
@@ -35,8 +41,7 @@
           <figure>img</figure>
           <div class="palacae-text-all">
             <p class="palace-h ebs">경복궁</p>
-            <p class="palace-name">덕수궁</p>
-            <p class="palace-name2">부제</p>
+            <p class="palace-name">부제</p>
             <p class="palacae-text">궁텍스트</p>
             <a class="palace-zoom" href="#">
               <p>자세히보기</p>
@@ -47,9 +52,8 @@
         <section class="palace palace3">
           <figure>img</figure>
           <div class="palacae-text-all">
-            <p class="palace-h ebs">경복궁</p>
-            <p class="palace-name">덕수궁</p>
-            <p class="palace-name2">부제</p>
+            <p class="palace-h ebs">덕수궁</p>
+            <p class="palace-name">부제</p>
             <p class="palacae-text">궁텍스트</p>
             <a class="palace-zoom" href="#">
               <p>자세히보기</p>
@@ -62,7 +66,7 @@
           <div class="palacae-text-all">
             <p class="palace-h ebs">경복궁</p>
             <p class="palace-name">덕수궁</p>
-            <p class="palace-name2">부제</p>
+            <p class="palace-name">부제</p>
             <p class="palacae-text">궁텍스트</p>
             <a class="palace-zoom" href="#">
               <p>자세히보기</p>
