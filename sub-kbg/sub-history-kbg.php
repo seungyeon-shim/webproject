4a5d53000aac85d3bf9bbf11fd5ea59d81a2a558
김소진 <thwls9930@gmail.com>
2023-01-11 20:12:25 +0900

    sub-history 학원에서 작업

--- sub-kbg/sub-history-kbg.php
+++ sub-kbg/sub-history-kbg.php
@@ -10,7 +10,6 @@
     <img class="frame1 frame1-left" src="./img/sub/sub-deco/frame1-left.png" alt="">
     <img class="frame1 frame1-right" src="./img/sub/sub-deco/frame1-right.png" alt="">
     <img class="cloud cloud1" src="./img/sub/sub-deco/cloud4.png" alt="">
-    <img class="cloud cloud2" src="./img/sub/sub-deco/cloud1.png" alt="">
     <img class="cloud cloud3" src="./img/sub/sub-deco/cloud4.png" alt="">
     <img class="lantern" src="./img/sub/sub-deco/lantern1.png" alt="">
   </figure>
@@ -19,34 +18,65 @@
     <div class="inner"><!-- 상단바  -->
       <img src="./img/sub/sub-history/kbg/history-bar.png" alt="">
       <div class="bar-textbox">
-        <div class="year1">
+        <div class="year year1 active">
           <p>1300 ~ 1399</p>
           <h3>1300</h3>
           <p class="point-sans">年</p>
         </div>
+        <div class="year year2">
+          <p>1400 ~ 1499</p>
+          <h3>1400</h3>
+          <p class="point-sans">年</p>
+        </div>
       </div>
-    </div><!-- 상단 바  -->
-    <div class="c2">
-      <div class="c2-textbox">
-      <i> > </i>
-      <h2> 제목</h2>
-      <p> 역사 어쩌구</p>
-      </div>
-      <figure>
-        <img src="" alt="">
-        <img src="" alt="">
-      </figure>
-    </div>
+    </div><!-- inner  -->
+
+    <div class="contents">
+      <div class="content content1"><!-- 컨텐츠 1개  -->
+        <div class="textbox">
+          <div class="wrap">
+            <img src="./img/sub/sub-deco/arrow.png" alt="">
+            <h2> 조선 건국과 조선의 법궁, 경복궁 창건</h2>
+          </div><!-- wrap -->
+          <p>1392년 &nbsp;&nbsp;조선 개국</p>
+          <p>1394년 &nbsp;&nbsp;개경에서 한양으로 천도</p>
+          <p>1395년 &nbsp;&nbsp;종묘·사직 그리고 경복궁 창건</p>
+        </div><!-- c2-textbox -->
+        <figure class="kbg-img">
+          <img class="kbg kbg1" src="./img/sub/sub-history/kbg/kbg-l1.png" alt="">
+          <img class="kbg kbg2" src="./img/sub/sub-history/kbg/kbg-s1.png" alt="">
+        </figure>
+      </div><!-- 컨텐츠1개 -->
+      <div class="content content2"><!-- 컨텐츠 1개  -->
+        <div class="textbox">
+          <div class="wrap">
+            <img src="./img/sub/sub-deco/arrow.png" alt="">
+            <h2> 궁궐의 상징성 보완과 법궁체제확립 </h2>
+          </div><!-- wrap -->
+          <p>1412년 &nbsp;&nbsp;경회루를 고쳐 지음</p>
+          <p>1427년 &nbsp;&nbsp;동궁 자선당 건립</p>
+          <p>1434년 &nbsp;&nbsp;광화문에 새 종 설치</p>
+          <p>1474년 &nbsp;&nbsp;근정전에 청기와 올림</p>
+        </div><!-- c2-textbox -->
+        <div class="img-box"></div>
+        <figure class="kbg-img">
+          <img class="kbg kbg1" src="./img/sub/sub-history/kbg/kbg-l2.png" alt="">
+          <img class="kbg kbg2" src="./img/sub/sub-history/kbg/kbg-s2.png" alt="">
+        </figure>
+      </div><!-- 컨텐츠 1개 -->
+    </div><!-- contents -->
     <div class="controls">
-      <div class="indicator">
-        <button>1300</button>
-        <button>1400</button>
-        <button>1500</button>
-        <button>1800</button>
-        <button>1900</button>
-        <button>2000</button>
-      </div><!-- indicator -->
-    </div><!-- controls -->
+    <button class="prev"><i class="fa-solid fa-chevron-left"></i></button>
+    <div class="indicator">
+      <button class="active">1300</button>
+      <button>1400</button>
+      <button>1500</button>
+      <button>1800</button>
+      <button>1900</button>
+      <button>2000</button>
+    </div><!-- indicator -->
+    <button class="next"><i class="fa-solid fa-chevron-right"></i></button>
+  </div><!-- controls -->
   </div><!-- content -->
 
 </section>
