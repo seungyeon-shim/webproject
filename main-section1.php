915f20fd020dea4d828ef7dc87c2628bd76ae01e
최준혁 <pass2149@gmail.com>
2023-01-11 19:01:15 +0900

    학원에서 메인 1섹션 수정 최준혁

--- main-section1.php
+++ main-section1.php
@@ -16,21 +16,25 @@
     <div class="sticky-inner">
       <div class="sticky-inner-container">
         <section class="palace palace1">
-        <div class="palace-name2 point-serif">景<br>福<br>宮</div>
           <figure>
-            <img class="cloud" src="./img/main/section1-about/kbg/kbg-cloud.png" alt="">
-            <img class="kbg" src="./img/main/section1-about/kbg/kbg-1.png" alt=""><!-- img -->
+            <div class="obj-wrap">
+              <img class="img-obj cloud" src="./img/main/section1-about/kbg/kbg-cloud.png" alt="">
+              <img class="img-palacae kbg" src="./img/main/section1-about/kbg/kbg-1.png" alt=""><!-- img -->
+            </div>
           </figure><!-- figure -->
           <div class="palacae-text-all">
             <p class="palace-name">조선왕조 제일의 법궁</p>
-            <p class="palace-h ebs">경복궁</p>
+            <h3 class="palace-h ebs">경복궁</h3>
             <p class="palacae-text">
-              조선건국과 함께 지어진 궁궐<br>
-              궁의 유례는 정도전의 시경의 한구절에서<br>
-              ‘큰복’을 의미하는 경복을 따왔습니다.<br>
-              수도 서울의 중심이고 조선의 으뜸 궁궐인<br>
-              경복궁에서 격조 높고 품위 있는 왕실 문화의 <br>
-              진수를 느껴보시기 바랍니다.</p>
+              조선건국과 함께 지어진 궁궐
+              <i class="tt-mob-none">
+                궁의 유례는 정도전의 시경의 한구절에서
+                ‘큰복’을 의미하는 경복을 따왔습니다.
+              </i>
+              수도 서울의 중심이고 조선의 으뜸 궁궐인
+              경복궁에서 격조 높고 품위 있는 왕실 문화의
+              진수를 느껴보시기 바랍니다.
+            </p>
             <a class="palace-zoom" href="#">
               <p>자세히보기 <i class="fa-solid fa-arrow-right"></i></p>
             </a><!-- palace-zoom -->
@@ -38,41 +42,84 @@
         </section><!-- palace1 경복궁 -->
 
         <section class="palace palace2">
-          <figure>img</figure>
+          <figure>
+            <div class="obj-wrap">
+              <img class="img-obj lantern" src="./img/main/section1-about/dsk/dsk-obj1.png" alt="">
+              <img class="img-obj lantern" src="./img/main/section1-about/dsk/dsk-obj2.png" alt="">
+              <img class="img-palacae dsk" src="./img/main/section1-about/dsk/dsk-1.png" alt=""><!-- img -->
+            </div>
+          </figure><!-- figure -->
           <div class="palacae-text-all">
-            <p class="palace-h ebs">경복궁</p>
-            <p class="palace-name">부제</p>
-            <p class="palacae-text">궁텍스트</p>
+            <p class="palace-name">조선왕조 제일의 법궁</p>
+            <h3 class="palace-h ebs">덕수궁</h3>
+            <p class="palacae-text">
+              조선건국과 함께 지어진 궁궐
+              <i class="tt-mob-none">
+                궁의 유례는 정도전의 시경의 한구절에서
+                ‘큰복’을 의미하는 경복을 따왔습니다.
+              </i>
+              수도 서울의 중심이고 조선의 으뜸 궁궐인
+              경복궁에서 격조 높고 품위 있는 왕실 문화의
+              진수를 느껴보시기 바랍니다.
+            </p>
             <a class="palace-zoom" href="#">
-              <p>자세히보기</p>
+              <p>자세히보기 <i class="fa-solid fa-arrow-right"></i></p>
             </a><!-- palace-zoom -->
-          </div><!-- 경복궁텍스트 -->
+          </div><!-- 덕수궁텍스트 -->
         </section><!-- palace2 덕수궁 -->
 
         <section class="palace palace3">
-          <figure>img</figure>
+          <figure>
+            <div class="obj-wrap">
+              <img class="img-obj moon" src="./img/main/section1-about/ckg/ckg-obj1.png" alt="">
+              <img class="img-palacae ckg" src="./img/main/section1-about/ckg/ckg-1.png" alt=""><!-- img -->
+            </div>
+          </figure><!-- figure -->
           <div class="palacae-text-all">
-            <p class="palace-h ebs">덕수궁</p>
-            <p class="palace-name">부제</p>
-            <p class="palacae-text">궁텍스트</p>
+            <p class="palace-name">왕들의 생활공간</p>
+            <h3 class="palace-h ebs">창경궁</h3>
+            <p class="palacae-text">
+              창경궁은 수강궁에서 창경원을 거쳐 창경궁이 되었다.
+              <i class="tt-mob-none">
+                창경궁에는 왕들의 지극한 효심과 사랑, 왕과 세자의 애증,
+                왕비와 후궁의 갈등 등 왕실 가족
+                사이에 일어난 이야기도 풍부하게 전해온다
+              </i>
+              영조와 사도세자의 이야기를 궁과함께 알아보자
+            </p>
             <a class="palace-zoom" href="#">
-              <p>자세히보기</p>
+              <p>자세히보기 <i class="fa-solid fa-arrow-right"></i></p>
             </a><!-- palace-zoom -->
-          </div><!-- 경복궁텍스트 -->
-        </section><!-- palace3 창경궁 -->
+          </div><!-- 창경궁텍스트 -->
+        </section><!-- palace3  -->
 
         <section class="palace palace4">
-          <figure>img</figure>
+          <figure>
+            <div class="obj-wrap">
+              <img class="img-obj sakura" src="./img/main/section1-about/cdg/cdg-obj1.png" alt="">
+              <img class="img-palacae cdg" src="./img/main/section1-about/cdg/cdg-1.png" alt=""><!-- img -->
+            </div>
+          </figure><!-- figure -->
           <div class="palacae-text-all">
-            <p class="palace-h ebs">경복궁</p>
-            <p class="palace-name">덕수궁</p>
-            <p class="palace-name">부제</p>
-            <p class="palacae-text">궁텍스트</p>
+            <p class="palace-name">자연과의 조화 세계문화유산</p>
+            <h3 class="palace-h ebs">창덕궁</h3>
+            <p class="palacae-text">
+              궁의 뜻은 덕의 근본을 밝혀 창성하게 되라는 뜻이다.
+              <i class="tt-mob-none">
+              조선시대의 뛰어난 조경을 보여주며 
+              300년이 넘는 나무와 비교적 원형이 충실히 보전 되어있다.
+              </i>
+              조선의 조경을 충실히 보존하고있는 
+              세계문화유산 을 느껴보시기 바랍니다.
+            </p>
             <a class="palace-zoom" href="#">
-              <p>자세히보기</p>
+              <p>자세히보기 <i class="fa-solid fa-arrow-right"></i></p>
             </a><!-- palace-zoom -->
-          </div><!-- 경복궁텍스트 -->
-        </section><!-- palace4 창덕궁 -->
+          </div><!-- 창덕궁텍스트 -->
+        </section><!-- palace4  -->
+
+
+
 
       </div><!--sticky-inner-container  -->
     </div><!--sticky-inner  -->
