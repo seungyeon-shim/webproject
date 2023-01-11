fc2052ce4594d621c89b3911f7e5f7c191e2b1b1
김도훈 <ehgns0908@naver.com>
2023-01-11 18:29:46 +0900

    오시는길 버스 지하철 정보 입력 및 디자인 내일은 스크롤 디자인 바꾸기

--- sub-kbg/sub-location-kbg.php
+++ sub-kbg/sub-location-kbg.php
@@ -15,31 +15,31 @@
       <div class="map-wrap">
         <div class="map">
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25295.6604935355!2d126.94202207910156!3d37.57961700000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca2c74aeddea1%3A0x8b3046532cc715f6!2z6rK967O16raB!5e0!3m2!1sko!2skr!4v1673347520949!5m2!1sko!2skr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-        </div>
-        <div class="bus">
+        </div><!-- map -->
 
-              <h3>버스안내</h3>
-              <h3>버스안내</h3>
-              <h3>버스안내</h3>
-              <h3>버스안내</h3>
-              <h3>버스안내</h3>
-              <h3>버스안내</h3>
-              <h3>버스안내</h3>
-              <h3>버스안내</h3>
-              <h3>버스안내</h3>
-              <h3>버스안내</h3>
-              <h3>버스안내</h3>
-              <h3>버스안내</h3>
-              <h3>버스안내</h3> 
-              <h3>버스안내</h3>
-              <h3>버스안내</h3>
-              <h3>버스안내</h3>
-              <h3>버스안내</h3>
-              <h3>버스안내</h3>
-              <h3>버스안내</h3>
-              <h3>버스안내</h3>
-  
-        </div>
+        <div class="transportation">
+
+         <div class="big-text">
+            <h2 class="ebs">경복궁</h2>
+              <h3>서울 종로구 사직로 161 경복궁</h3>
+         </div>
+           
+          <div class="kbg-trans">
+            <div class="subway">
+              <h3><i class="fa-solid fa-train-subway"></i> 지하철로 오시는길</h3>
+                <li><p class="orange"> 3호선 </p><em>경복궁역 5번출구 도보 5분</em></li>
+                <li><p class="purple"> 5호선 </p><em>광화문역 2번출구 도보 약 10분</em></li>
+            </div>
+            <div class="bus">
+              <h3><i class="fa-solid fa-bus-simple"></i> 버스로 오시는길</h3>
+              <li><p class="green">마을</p><em>종로11 | 종로9 | 종로01</em></li>
+                <li><p class="blue">간선</p><em>109 | 171 | 272 | 601</em></li>
+            </div>
+          </div><!-- kbg-trans -->
+
+
+        </div><!-- transportation -->
+
       </div><!-- map-wrap -->
     </div><!-- paper-wrap -->
     <img class="grap grap-land grap-left" src="./img/sub/sub-location/scroll-paper-grip.png" alt="">
@@ -47,6 +47,6 @@
     <img class="grap grap-port grap-top" src="./img/sub/sub-location/scroll-paper-grip-v.png" alt="">
     <img class="grap grap-port grap-bottom" src="./img/sub/sub-location/scroll-paper-grip-v.png" alt="">
 
-  </div>
+  </div><!-- location-wrap -->
 
 </section>
