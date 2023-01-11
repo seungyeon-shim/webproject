e3b900cd39793df5a78ec9d51df59f5c5358690f
한주은 <nini0221@naver.com>
2023-01-11 16:28:28 +0900

    23.01.11 4:28 수정

--- sub-kbg/sub-story-kbg.php
+++ sub-kbg/sub-story-kbg.php
@@ -15,22 +15,30 @@
 
     <div class="sub-middle-title-box">
         
+        <img class="folding-screen" src="./img/sub/sub-deco/folding-screen.png" alt="">
+
         <div class="sub-middle-title sub-middle-title1">
+            <img class="story-hover-bg" src="./img/sub/sub-deco/story-hover-bg.png" alt="">
             <p class="sub-first-desc">명성황후 시해</p>
             <p class="sub-second-desc">향원정 북쪽에 있는 건청궁 <br> 곤녕합에서 일본인들에게 시해</p>
-            <img src="./img/sub/sub-story/kbg/original/1.png" alt="">
+            <img class="text-bg text-bg1" src="./img/sub/sub-deco/text-bg.png" alt="">
+            <img class="sub-img sub-first-img" src="./img/sub/sub-story/kbg/original/1.png" alt="">
         </div><!-- sub-middle-title1 -->
 
         <div class="sub-middle-title sub-middle-title2">
+            <img class="story-hover-bg" src="./img/sub/sub-deco/story-hover-bg.png" alt="">
             <p class="sub-first-desc">오래 머물렀던 왕</p>
             <p class="sub-second-desc">세종대왕은 경복궁에서 오래 머무르면서 <br> 한글을 창제 후 과학기술 발전 도모</p>
-            <img src="./img/sub/sub-story/kbg/original/2.png" alt="">
+            <img class="text-bg text-bg2" src="./img/sub/sub-deco/text-bg.png" alt="">
+            <img class="sub-img sub-second-img" src="./img/sub/sub-story/kbg/original/2.png" alt="">
         </div><!-- sub-middle-title2 -->
 
         <div class="sub-middle-title sub-middle-title3">
+            <img class="story-hover-bg" src="./img/sub/sub-deco/story-hover-bg.png" alt="">
             <p class="sub-first-desc">경회루 연못의 용</p>
             <p class="sub-second-desc">경회루 연못에서 청동으로 만든 <br> 용(龍) 두 마리가 발견</p>
-            <img src="./img/sub/sub-story/kbg/original/3.png" alt="">
+            <img class="text-bg text-bg3" src="./img/sub/sub-deco/text-bg.png" alt="">
+            <img class="sub-img sub-third-img" src="./img/sub/sub-story/kbg/original/3.png" alt="">
         </div><!-- sub-middle-title3 -->
 
     </div><!-- sub-middle-title-box -->
