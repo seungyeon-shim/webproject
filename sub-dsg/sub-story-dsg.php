eeb6d92f4a6a7d947b1f11884928f0ae1aeb543d
한주은 <nini0221@naver.com>
2023-01-12 20:36:02 +0900

    덕수궁 이야기 텍스트 수정

--- sub-dsg/sub-story-dsg.php
+++ sub-dsg/sub-story-dsg.php
@@ -1,3 +1,130 @@
-<section class="sub-content sub-story sub-story-dsg">
-    <h3>덕수궁 이야기</h3>
+<link rel="stylesheet" href="./css/sub-kbg/sub-story.css">
+<script src="./js/sub-story/sub-story.js"></script>
+<section class="sub-content sub-story sub-story-kbg sub-content-center">
+
+  <img class="deco back-pattern" src="./img/sub/sub-deco/back-pattern.png" alt="">
+  <img class="deco story-left" src="./img/sub/sub-deco/story-left.png" alt="">
+  <img class="deco story-right" src="./img/sub/sub-deco/story-right.png" alt="">
+
+  <figure class="wave"></figure>
+
+  <div class="sub-title-wrap sub-title-wrap-center">
+    <p class="sub-desc">대한제국의 황궁</p>
+    <h2 class="sub-title">덕수궁 이야기</h2>
+  </div>
+
+  <div class="content">
+    <div class="box box1 active" data-n="1">
+      <div class="flip">
+        <div class="face front">
+          <img class="screen" src="./img/sub/sub-deco/folding-screen1.png" alt="">
+          <h3 class="ebs">
+            <img class="brush" src="./img/sub/sub-deco/title-top.png" alt="">
+            <span>대화재 사건</span>
+          </h3>
+          <p>
+            <img class="brush" src="./img/sub/sub-deco/title-bottom.png" alt="">
+            <span>
+            1904년 4월 14일 22시 <br>
+            함녕전의 동온돌에서 화재 발생
+            </span>
+          </p>
+          <img class="main" src="./img/sub/sub-story/dsg/original/1.png" alt="">
+        </div><!-- front -->
+        <div class="face back">
+          <img src="./img/sub/sub-deco/folding-screen1-back.png" alt=""> 
+        </div><!-- back -->
+        
+      </div><!-- flip -->
+    </div><!-- box1 -->
+
+    <div class="box box2" data-n="2">
+      <div class="flip">
+        <div class="face front">
+        <img class="screen" src="./img/sub/sub-deco/folding-screen2.png" alt="">
+          <h3 class="ebs">
+            <img class="brush" src="./img/sub/sub-deco/title-top.png" alt="">
+            <span>인목대비와 경운궁</span>
+          </h3>
+          <p>
+            <img class="brush" src="./img/sub/sub-deco/title-bottom.png" alt="">
+            <span>
+            임진왜란으로 인해 궁이 되었던 <br>
+            경운궁은 역사 속으로 소산.
+            </span>
+          </p>
+          <img class="main" src="./img/sub/sub-story/dsg/original/2.png" alt="">
+        </div><!-- front -->
+        <div class="face back">
+        <img src="./img/sub/sub-deco/folding-screen2-back.png" alt="">
+        </div><!-- back -->
+      </div><!-- flip -->
+    </div><!-- box2 -->
+
+    <div class="box box3" data-n="3">
+      <div class="flip">
+        <div class="face front">
+        <img class="screen" src="./img/sub/sub-deco/folding-screen3.png" alt="">
+          <h3 class="ebs">
+            <img class="brush" src="./img/sub/sub-deco/title-top.png" alt="">
+            <span>을사조약 체결</span>
+          </h3>
+          <p>
+            <img class="brush" src="./img/sub/sub-deco/title-bottom.png" alt="">
+            <span>
+            1905년 일본이 한국의 외교권을 <br>
+            박탈하기 위해 강제로 체결한 조약
+            </span>
+          </p>
+          <img class="main" src="./img/sub/sub-story/dsg/original/3.png" alt="">
+        </div><!-- front -->
+        <div class="face back">
+          <img src="./img/sub/sub-deco/folding-screen3-back.png" alt="">
+        </div><!-- back -->
+      </div><!-- flip -->
+    </div><!-- box3 -->
+    <button class="indicator prev" data-n="-1"><i class="fa-solid fa-angle-left"></i></button>
+    <button class="indicator next" data-n="1"><i class="fa-solid fa-angle-right"></i></button>
+  </div><!-- content -->
+ 
+  <div class="open-container">
+      <div class="open open1">
+        <h4 class="ebs">대화재 사건</h4>
+        <p>
+        일본공사관이 소방관을 예견한 것 처럼 파견한 내용이 담긴
+        ‘퐁트네보고서’ 가 러시아 외교문서고에서 발견. 프랑스 대리공사
+        퐁트네를 통해 고종은 니콜라이 러시아 황제에게 친서 전달. 그 친서에 
+        ‘일본군은 경운궁을 방화하여 나를 불태워 죽이려 했으며 내 눈앞에서
+        일본군이 범궐하여 한국 장교들이 피살되는 것을 직접 보았다’ 고
+        기술. 일본공사 하야시는 고종황제에게 경복궁으로 이어를 강권했지만
+        극력 거부한 고종을 태워 죽이려고 경운궁을 방화.
+        </p>
+        <img src="./img/sub/sub-story/dsg/hover/1.png" alt="">
+      </div>
+      <div class="open open2">
+      <h4 class="ebs">인목대비와 경운궁</h4>
+        <p>
+        1618년, 광해군은 인목대비를 폐위시켜 경운궁에 유폐시키고
+        경운궁을 서궁이라 낮추어 호칭. 1623년 인조반정으로
+        광해군이 폐위되고, 인조는 인목대비를 찾아와
+        즉위식을 갖고 왕위에 등극. 그후 인조는 인목대비를 모시고
+        창덕궁으로 이어하고, 선조가 머물던 즉조당과 석어당만을
+        남기고 경운궁의 나머지 땅과 집을 모두 원래 주인에게 환부.
+        </p>
+        <img src="./img/sub/sub-story/dsg/hover/2.png" alt="">
+      </div>
+      <div class="open open3">
+      <h4 class="ebs">을사조약 체결</h4>
+        <p>
+        ‘을사조약'은 1905년 일본이 한국의 외교권을 박탈하기 위해
+        강제로 체결한 조약으로, 상호 간에 맺은 조약이 아닌 강제적으로
+        맺은 조약이란 뜻의 ‘을사늑약’이라고도 한다. 특히 5인의
+        친일파에 의해 맺어진 조약이라 '을사 5적’이라는 명칭 갖는다. 
+        박제순, 이지용, 이근택, 이완용, 권중현을 지칭. 이들이
+        조약을 찬성했기에 터무니 없는 조약이 체결. 을사조약이 덕수궁
+        중명전에서 체결되며 나라를 잃게 되는 역사의 장소가 된다.
+        </p>
+        <img src="./img/sub/sub-story/dsg/hover/3.png" alt="">
+      </div>
+    </div><!-- open-container -->
 </section>
