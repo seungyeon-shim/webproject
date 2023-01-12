27743f51c6fde00d93543f822b25a754e4578097
김소진 <thwls9930@gmail.com>
2023-01-12 20:45:19 +0900

    sub-history 학원에서 작업 구조 설계 , 위치배치

---
+++ js/sub-history/store.js
@@ -0,0 +1,89 @@
+historyArr1 = [
+  {//v
+    century : '1300 ~ 1399',
+    year : '1300',
+    arrow : 'arrow.png',
+    title : '조선 건국과 조선의 법궁, 경복궁 창건',
+    yearlyArr : [//ul
+      '<em>1392년</em> <b>조선 개국</b>',//il m
+      '<em>1394년</em> <b>개경에서 한양으로 천도</b>',
+      '<em>1395년</em> <b>종묘·사직 그리고 경복궁 창건</b>',
+    ],
+    imgArr : [
+      'kbg-l1.png','kbg-s1.png'
+    ]   
+  },
+  {//v
+    century : '1400 ~ 1499',
+    year : '1400',
+    arrow : 'arrow.png',
+    title : '경복궁 법궁체제확립',
+    yearlyArr : [//ul
+      '<em>1431년</em> <b>광화문을 고쳐 지음</b>',//il m
+      '<em>1434년</em> <b>광화문에 새 종 설치</b>',
+      '<em>1443년</em> <b>계조당·교태전 새로 지음</b>',
+    ],
+    imgArr : [
+      'kbg-l2.png','kbg-s2.png'
+    ]   
+  },
+  {//v
+    century : '1500 ~ 1599',
+    year : '1500',
+    arrow : 'arrow.png',
+    title : '화재와 임진왜란으로의 소실',
+    yearlyArr : [//ul
+      '<em>1543년</em> <b>동궁 화재로 인해 소실</b>',//il m
+      '<em>1553년</em> <b>강녕전, 사정전 등 화재로 소실</b>',
+      '<em>1592년</em> <b>임진왜란으로 경복궁 소실</b>',
+    ],
+    imgArr : [
+      'kbg-l3.png','kbg-s3.png'
+    ]   
+  },
+  {//v
+    century : '1800 ~ 1899',
+    year : '1800',
+    arrow : 'arrow.png',
+    title : '대원군의 중건과 화재',
+    yearlyArr : [//ul
+      '<em>1865년</em> <b>경복궁 중건 시작</b>',//il m
+      '<em>1867년</em> <b>경복궁 중건 완료</b>',
+      '<em>1876년</em> <b>화재로 830여칸 건물 소실</b>',
+      '<em>1888년</em> <b>강녕전·교태전·자경전 등 재건</b>',
+    ],
+    imgArr : [
+      'kbg-l4.png','kbg-s4.png'
+    ]   
+  },
+  {//v
+    century : '1900 ~ 1999',
+    year : '1900',
+    arrow : 'arrow.png',
+    title : '민족 수난기와 경복궁 복원',
+    yearlyArr : [//ul
+      '<em>1915년</em> <b>조선총독부 박물관 건물 준공</b>',//il m
+      '<em>1927년</em> <b>조선총독부 청사 준공</b>',
+      '<em>1968년</em> <b>광화문 복원</b>',
+      '<em>1997년</em> <b>구 조선총독부 청사 철거</b>',
+    ],
+    imgArr : [
+      'kbg-l5.png','kbg-s5.png'
+    ]   
+  },
+  {//v
+    century : '2000 ~ 2023',
+    year : '2000',
+    arrow : 'arrow.png',
+    title : '경복궁 복원',
+    yearlyArr : [//ul
+      '<em>2001년</em> <b>흥례문·유화문·영제교 복원</b>',//il m
+      '<em>2010년</em> <b>광화문 일곽 복원</b>',
+      '<em>2021년</em> <b>향원정 보수 및 취향교 복원</b>',
+      '<em>현재	</em> <b>계조당 권역 복원 진행중</b>',
+    ],
+    imgArr : [
+      'kbg-l6.png','kbg-s6.png'
+    ]   
+  },
+]
--- sub-kbg/sub-history-kbg.php
+++ sub-kbg/sub-history-kbg.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="./css/sub-kbg/sub-history.css">
+<script src="./js/sub-history/store.js"></script>
 <section class="sub-content sub-history sub-history-kbg sub-content-center">
   <div class="sub-title-wrap sub-title-wrap-center">
     <h2 class="sub-title">경복궁 역사</h2>
@@ -15,68 +16,62 @@
   </figure>
 
   <div class="sub-history-content"><!-- 전체 -->
-    <div class="inner"><!-- 상단바  -->
-      <img src="./img/sub/sub-history/kbg/history-bar.png" alt="">
-      <div class="bar-textbox">
-        <div class="year year1 active">
-          <p>1300 ~ 1399</p>
-          <h3>1300</h3>
-          <p class="point-sans">年</p>
-        </div>
-        <div class="year year2">
-          <p>1400 ~ 1499</p>
-          <h3>1400</h3>
-          <p class="point-sans">年</p>
-        </div>
-      </div>
-    </div><!-- inner  -->
-
-    <div class="contents">
-      <div class="content content1"><!-- 컨텐츠 1개  -->
-        <div class="textbox">
-          <div class="wrap">
-            <img src="./img/sub/sub-deco/arrow.png" alt="">
-            <h2> 조선 건국과 조선의 법궁, 경복궁 창건</h2>
-          </div><!-- wrap -->
-          <p>1392년 &nbsp;&nbsp;조선 개국</p>
-          <p>1394년 &nbsp;&nbsp;개경에서 한양으로 천도</p>
-          <p>1395년 &nbsp;&nbsp;종묘·사직 그리고 경복궁 창건</p>
-        </div><!-- c2-textbox -->
-        <figure class="kbg-img">
-          <img class="kbg kbg1" src="./img/sub/sub-history/kbg/kbg-l1.png" alt="">
-          <img class="kbg kbg2" src="./img/sub/sub-history/kbg/kbg-s1.png" alt="">
-        </figure>
-      </div><!-- 컨텐츠1개 -->
-      <div class="content content2"><!-- 컨텐츠 1개  -->
-        <div class="textbox">
-          <div class="wrap">
-            <img src="./img/sub/sub-deco/arrow.png" alt="">
-            <h2> 궁궐의 상징성 보완과 법궁체제확립 </h2>
-          </div><!-- wrap -->
-          <p>1412년 &nbsp;&nbsp;경회루를 고쳐 지음</p>
-          <p>1427년 &nbsp;&nbsp;동궁 자선당 건립</p>
-          <p>1434년 &nbsp;&nbsp;광화문에 새 종 설치</p>
-          <p>1474년 &nbsp;&nbsp;근정전에 청기와 올림</p>
-        </div><!-- c2-textbox -->
-        <div class="img-box"></div>
-        <figure class="kbg-img">
-          <img class="kbg kbg1" src="./img/sub/sub-history/kbg/kbg-l2.png" alt="">
-          <img class="kbg kbg2" src="./img/sub/sub-history/kbg/kbg-s2.png" alt="">
-        </figure>
-      </div><!-- 컨텐츠 1개 -->
-    </div><!-- contents -->
-    <div class="controls">
-    <button class="prev"><i class="fa-solid fa-chevron-left"></i></button>
-    <div class="indicator">
-      <button class="active">1300</button>
-      <button>1400</button>
-      <button>1500</button>
-      <button>1800</button>
-      <button>1900</button>
-      <button>2000</button>
-    </div><!-- indicator -->
-    <button class="next"><i class="fa-solid fa-chevron-right"></i></button>
-  </div><!-- controls -->
-  </div><!-- content -->
-
+    <!-- 상단 바 이미지 -->
+    <div class="inner">
+      <div class="bar-inner">
+        <img src="./img/sub/sub-history/kbg/history-bar.png" alt="">
+        <ol class="century-box"></ol>
+      </div><!-- inner -->
+      <script>
+        historyArr1.forEach(function(v) {
+          $(`.sub-history-content .inner ol`).append(`
+          <li>
+            <p class="century">${v.century}</p>
+            <h3 class="year">${v.year}<span class="point-sans">年</span></h3>
+          </li>
+        `)
+        })
+      </script>
+      <ol class="main-content"></ol>
+      </div><!-- 슬라이드 용 div -->
+      <script>
+        historyArr1.forEach(function(e) {
+          $(`.sub-history-content .main-content`).append(`
+        <li>
+          <div>
+            <h4>
+              <img class="arrow" src="./img/sub/sub-deco/${e.arrow}" alt="">
+              <em class="history-title">${e.title}</em>
+            </h4>
+            <ul class="yearly-textbox"></ul>
+          </div>
+          <figure>
+          </figure>
+        </li> 
+      `) //history1 append
+          e.yearlyArr.forEach(function(m) {
+            $(`.main-content > li:last-child > div > ul`).append(`
+          <li>${m}</li>
+        `)
+          }) //monthArr
+          e.imgArr.forEach(function(i) {
+            $(`.main-content > li:last-child > figure`).append(`
+          <img src="./img/sub/sub-history/kbg/${i}" alt="">
+        `)
+          }) //imgArr
+        }) //historyArr1
+      </script>
+      <div class="controls">
+        <button class="prev"><i class="fa-solid fa-chevron-left"></i></button>
+        <div class="indicator">
+          <button class="active">1300</button>
+          <button>1400</button>
+          <button>1500</button>
+          <button>1800</button>
+          <button>1900</button>
+          <button>2000</button>
+        </div> <!-- indicator  -->
+        <button class="next"><i class="fa-solid fa-chevron-right"></i></button>
+      </div> <!-- controls  -->
+  </div><!-- content  -->
 </section>
