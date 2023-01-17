041c89baeb9eb2af91f84694dffd0ab03cf89ca0
김도훈 <ehgns0908@naver.com>
2023-01-17 19:01:29 +0900

    //덕수궁 비주얼 로케이션

--- sub-dsg/sub-location-dsg.php
+++ sub-dsg/sub-location-dsg.php
@@ -1,11 +1,72 @@
-<section class="sub-content sub-location sub-location-dsg">
-  <div class="sub-title-wrap">
-    <h3>덕수궁 오시는길</h3>
-    <p>
-      평화통일정책의 수립에 관한 대통령의 자문에 응하기 위하여 민주평화통일자문회의를 둘 수 있다. 
-      학교교육 및 평생교육을 포함한 교육제도와 그 운영, 교육재정 및 교원의 지위에 관한 기본적인 사항은 법률로 정한다.
+<link rel="stylesheet" href="./css/sub-kbg/sub-location.css">
+<link rel="stylesheet" href="./css/scrollbar.css">
+<script src="./js/sub-location/sub-location.js"></script>
+
+<section class="sub-content sub-location sub-location-kbg sub-content-center">
+
+  <img class="location-bg" src="./img/sub/sub-location/location-bg.png" alt="">
+  <img class="mount" src="./img/sub/sub-location/location-mount.png" alt="">
+  <img class="texture" src="./img/sub/sub-deco/texture-bg.png" alt="">
+  <img class="left" src="./img/sub/sub-deco/frame2-left.png" alt="">
+  <img class="right" src="./img/sub/sub-deco/frame2-right.png" alt="">
+
+
+  <div class="sub-title-wrap  sub-title-wrap-center">
+    <h2 class="sub-title">덕수궁 오시는길</h2>
+    <p class="sub-desc">
+    고궁의 고즈넉함을 온전히 느낄 수 있는 덕수궁으로 오세요
     </p>
   </div><!-- sub-title-wrap -->
-  
-  
+
+  <div class="location-wrap">
+    <div class="paper-wrap">
+      <img class="scr-paper scr-paper-land" src="./img/sub/sub-location/scroll-paper.png" alt="">
+      <img class="scr-paper scr-paper-port" src="./img/sub/sub-location/scroll-paper-v.png" alt="">
+      <div class="map-wrap">
+        <div class="map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1581.2736824139524!2d126.97341795825807!3d37.565726671015426!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca28d3199c531%3A0x7a2d35df0efd2d8!2z642V7IiY6raB!5e0!3m2!1sko!2skr!4v1673948351257!5m2!1sko!2skr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div><!-- map -->
+
+
+        <div class="transportation" id="style-4">
+          
+          <div class="big-text">
+            <h2 class="ebs">덕수궁</h2>
+            <h3>서울 종로구 사직로 161 경복궁</h3>
+          </div>
+
+          <div class="kbg-trans">
+            <div class="subway">
+              <h3><i class="fa-solid fa-train-subway"></i> 지하철로 오시는길</h3>
+              <li>
+                <p class="orange"> 3호선 </p><em>경복궁역 5번출구 도보 5분</em>
+              </li>
+              <li>
+                <p class="purple"> 5호선 </p><em>광화문역 2번출구 도보 약 10분</em>
+              </li>
+            </div>
+            <div class="bus">
+              <h3><i class="fa-solid fa-bus-simple"></i> 버스로 오시는길</h3>
+              <li>
+                <p class="green">마을</p><em>종로11 | 종로9 | 종로01</em>
+              </li>
+              <li>
+                <p class="blue">간선</p><em>109 | 171 | 272 | 601</em>
+              </li>
+            </div>
+          </div><!-- kbg-trans -->
+
+
+        </div><!-- transportation -->
+       
+
+      </div><!-- map-wrap -->
+    </div><!-- paper-wrap -->
+    <img class="grap grap-land grap-left" src="./img/sub/sub-location/scroll-paper-grip.png" alt="">
+    <img class="grap grap-land grap-right" src="./img/sub/sub-location/scroll-paper-grip.png" alt="">
+    <img class="grap grap-port grap-top" src="./img/sub/sub-location/scroll-paper-grip-v.png" alt="">
+    <img class="grap grap-port grap-bottom" src="./img/sub/sub-location/scroll-paper-grip-v.png" alt="">
+
+  </div><!-- location-wrap -->
+
 </section>
