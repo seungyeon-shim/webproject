0ff2f9ff5d28bc795bcc63d104ce280681ccae3e
sowon <thwls9930@gmail.com>
2023-01-19 00:04:18 +0900

    덕수궁,창경궁,창덕궁 역사 페이지 작업

--- js/sub-history/store.js
+++ js/sub-history/store.js
@@ -1,14 +1,13 @@
 /* kbgarr------------------------------------------------ */
-
 kbgArr = [
-  {//v
+  {
     century : '1300 ~ 1399',
     year : '1300',
     slideImg : 'slide-img1.png',
     arrow : 'arrow.png',
     title : '조선 건국과 조선의 법궁, 경복궁 창건',
-    yearlyArr : [//ul
-      '<em>1392년</em> <b>조선 개국</b>',//il m
+    yearlyArr : [
+      '<em>1392년</em> <b>조선 개국</b>',
       '<em>1394년</em> <b>개경에서 한양으로 천도</b>',
       '<em>1395년</em> <b>종묘·사직과 경복궁 창건</b>',
     ],
@@ -16,14 +15,14 @@ kbgArr = [
       'kbg-l1.png','kbg-s1.png'
     ]   
   },
-  {//v
+  {
     century : '1400 ~ 1499',
     year : '1400',
     slideImg : 'slide-img2.png',
     arrow : 'arrow.png',
     title : '경복궁 법궁체제확립',
-    yearlyArr : [//ul
-      '<em>1431년</em> <b>광화문을 고쳐 지음</b>',//il m
+    yearlyArr : [
+      '<em>1431년</em> <b>광화문을 고쳐 지음</b>',
       '<em>1434년</em> <b>광화문에 새 종 설치</b>',
       '<em>1443년</em> <b>계조당·교태전 새로 지음</b>',
     ],
@@ -31,14 +30,14 @@ kbgArr = [
       'kbg-l2.png','kbg-s2.png'
     ]   
   },
-  {//v
+  {
     century : '1500 ~ 1599',
     year : '1500',
     slideImg : 'slide-img3.png',
     arrow : 'arrow.png',
     title : '화재와 임진왜란으로의 소실',
-    yearlyArr : [//ul
-      '<em>1543년</em> <b>동궁 화재로 인해 소실</b>',//il m
+    yearlyArr : [
+      '<em>1543년</em> <b>동궁 화재로 인해 소실</b>',
       '<em>1553년</em> <b>강녕전, 사정전 등 화재로 소실</b>',
       '<em>1592년</em> <b>임진왜란으로 경복궁 소실</b>',
     ],
@@ -46,14 +45,14 @@ kbgArr = [
       'kbg-l3.png','kbg-s3.png'
     ]   
   },
-  {//v
+  {
     century : '1800 ~ 1899',
     year : '1800',
     slideImg : 'slide-img4.png',
     arrow : 'arrow.png',
     title : '대원군의 중건과 화재',
-    yearlyArr : [//ul
-      '<em>1865년</em> <b>경복궁 중건 시작</b>',//il m
+    yearlyArr : [
+      '<em>1865년</em> <b>경복궁 중건 시작</b>',
       '<em>1867년</em> <b>경복궁 중건 완료</b>',
       '<em>1876년</em> <b>화재로 830여칸 건물 소실</b>',
       '<em>1888년</em> <b>강녕전·교태전·자경전 등 재건</b>',
@@ -62,14 +61,14 @@ kbgArr = [
       'kbg-l4.png','kbg-s4.png'
     ]   
   },
-  {//v
+  {
     century : '1900 ~ 1999',
     year : '1900',
     slideImg : 'slide-img5.png',
     arrow : 'arrow.png',
     title : '민족 수난기와 경복궁 복원',
-    yearlyArr : [//ul
-      '<em>1915년</em> <b>조선총독부 박물관 건물 준공</b>',//il m
+    yearlyArr : [
+      '<em>1915년</em> <b>조선총독부 박물관 건물 준공</b>',
       '<em>1927년</em> <b>조선총독부 청사 준공</b>',
       '<em>1968년</em> <b>광화문 복원</b>',
       '<em>1997년</em> <b>구 조선총독부 청사 철거</b>',
@@ -78,14 +77,14 @@ kbgArr = [
       'kbg-l5.png','kbg-s5.png'
     ]   
   },
-  {//v
+  {
     century : '2000 ~ 2023',
     year : '2000',
     slideImg : 'slide-img6.png',
     arrow : 'arrow.png',
     title : '계속되는 경복궁 복원',
-    yearlyArr : [//ul
-      '<em>2001년</em> <b>흥례문·유화문·영제교 복원</b>',//il m
+    yearlyArr : [
+      '<em>2001년</em> <b>흥례문·유화문·영제교 복원</b>',
       '<em>2010년</em> <b>광화문 일곽 복원</b>',
       '<em>2021년</em> <b>향원정 보수 및 취향교 복원</b>',
       '<em>현재	</em> <b>계조당 권역 복원 진행중</b>',
@@ -95,100 +94,289 @@ kbgArr = [
     ]   
   },
 ]/* kbgarr---------------------------------------------- */
+
+
 /* dsgarr----------------------------------------------- */
 dsgArr = [
   {
-    century : '1300 ~ 1399',
-    year : '1300',
-    slideImg : 'slide-img1.png',
-    arrow : 'arrow.png',
-    title : '조선 건국과 조선의 법궁, 경복궁 창건',
-    yearlyArr : [//ul
-      '<em>1392년</em> <b>조선 개국</b>',//il m
-      '<em>1394년</em> <b>개경에서 한양으로 천도</b>',
-      '<em>1395년</em> <b>종묘·사직과 경복궁 창건</b>',
-    ],
-    imgArr : [
-      'kbg-l1.png','kbg-s1.png'
-    ]   
-  },
-  {//v
     century : '1400 ~ 1499',
     year : '1400',
-    slideImg : 'slide-img2.png',
+    slideImg : 'slide-img1.png',
     arrow : 'arrow.png',
-    title : '경복궁 법궁체제확립',
-    yearlyArr : [//ul
-      '<em>1431년</em> <b>광화문을 고쳐 지음</b>',//il m
-      '<em>1434년</em> <b>광화문에 새 종 설치</b>',
-      '<em>1443년</em> <b>계조당·교태전 새로 지음</b>',
+    title : ' 덕수궁의 승계',
+    yearlyArr : [
+      '<em>1469년</em> <b>덕수궁 터 몰수</b>',
+      '<em>1470년</em> <b>세종의 막내아들에게 하사</b>',
+      '<em>1472년</em> <b> 월산대군에게 하사</b>',
     ],
     imgArr : [
-      'kbg-l2.png','kbg-s2.png'
+      'dsg-l1.png','dsg-s1.png'
     ]   
   },
-  {//v
+  {
     century : '1500 ~ 1599',
     year : '1500',
+    slideImg : 'slide-img2.png',
+    arrow : 'arrow.png',
+    title : '행궁의 시작',
+    yearlyArr : [
+      '<em>1592년</em> <b>덕수궁을 시어소로 정함</b>',
+      '<em>1593년</em> <b>덕수궁을 행궁으로 삼음</b>'
+    ],
+    imgArr : [
+      'dsg-l2.png','dsg-s2.png'
+    ]   
+  },
+  {
+    century : '1600 ~ 1699',
+    year : '1600',
     slideImg : 'slide-img3.png',
     arrow : 'arrow.png',
-    title : '화재와 임진왜란으로의 소실',
-    yearlyArr : [//ul
-      '<em>1543년</em> <b>동궁 화재로 인해 소실</b>',//il m
-      '<em>1553년</em> <b>강녕전, 사정전 등 화재로 소실</b>',
-      '<em>1592년</em> <b>임진왜란으로 경복궁 소실</b>',
+    title : '광해군 즉위와 대비 유폐',
+    yearlyArr : [
+      '<em>1608년</em> <b>광해군이 정릉동 서청에서 즉위</b>',
+      '<em>1611년</em> <b>정릉동을 ‘경운궁’이라 고침</b>',
+      '<em>1618년 </em> <b>인목대비를 유폐</b>',
+      '<em>1623년 </em> <b>경운궁 별당에서 광해군 즉위</b>',
     ],
     imgArr : [
-      'kbg-l3.png','kbg-s3.png'
+      'dsg-l3.png','dsg-s3.png'
     ]   
   },
-  {//v
-    century : '1800 ~ 1899',
-    year : '1800',
+  {
+    century : '1700 ~ 1799',
+    year : '1700',
     slideImg : 'slide-img4.png',
     arrow : 'arrow.png',
-    title : '대원군의 중건과 화재',
-    yearlyArr : [//ul
-      '<em>1865년</em> <b>경복궁 중건 시작</b>',//il m
-      '<em>1867년</em> <b>경복궁 중건 완료</b>',
-      '<em>1876년</em> <b>화재로 830여칸 건물 소실</b>',
-      '<em>1888년</em> <b>강녕전·교태전·자경전 등 재건</b>',
+    title : '즉조당의 쓰임새 ',
+    yearlyArr : [
+      '<em>1769년</em> <b>광해군이 즉조당에 친필을 게판</b>',
+      '<em>1773년</em> <b>석어당 현판을 즉조당에 설치</b>'
     ],
     imgArr : [
-      'kbg-l4.png','kbg-s4.png'
+      'dsg-l4.png','dsg-s4.png'
     ]   
   },
-  {//v
-    century : '1900 ~ 1999',
-    year : '1900',
+  {
+    century : '1800 ~ 1899',
+    year : '1800',
     slideImg : 'slide-img5.png',
     arrow : 'arrow.png',
-    title : '민족 수난기와 경복궁 복원',
-    yearlyArr : [//ul
-      '<em>1915년</em> <b>조선총독부 박물관 건물 준공</b>',//il m
-      '<em>1927년</em> <b>조선총독부 청사 준공</b>',
-      '<em>1968년</em> <b>광화문 복원</b>',
-      '<em>1997년</em> <b>구 조선총독부 청사 철거</b>',
+    title : '을미사변과 대한제국 선포',
+    yearlyArr : [
+      '<em>1897년</em> <b>경운궁으로 환어</b>',
+      '<em>1897년</em> <b>대한제국을 선포</b>',
+      '<em>1899년</em> <b>중명전 건립</b>',
     ],
     imgArr : [
-      'kbg-l5.png','kbg-s5.png'
+      'dsg-l5.png','dsg-s5.png'
     ]   
   },
-  {//v
-    century : '2000 ~ 2023',
-    year : '2000',
+  {
+    century : '1900 ~ 1999',
+    year : '1900',
     slideImg : 'slide-img6.png',
     arrow : 'arrow.png',
-    title : '계속되는 경복궁 복원',
-    yearlyArr : [//ul
-      '<em>2001년</em> <b>흥례문·유화문·영제교 복원</b>',//il m
-      '<em>2010년</em> <b>광화문 일곽 복원</b>',
-      '<em>2021년</em> <b>향원정 보수 및 취향교 복원</b>',
-      '<em>현재	</em> <b>계조당 권역 복원 진행중</b>',
+    title : '석조전 건설',
+    yearlyArr : [
+      '<em>1905년</em> <b>수옥헌에서 을사늑약 체결</b>',
+      '<em>1907년</em> <b>경운궁을 덕수궁이라 명명</b>',
+      '<em>1910년</em> <b>덕수궁 내에 석조전 완공</b>',
+      '<em>1935년</em> <b>돈덕전 자리에 동물원 신설</b>',
     ],
     imgArr : [
-      'kbg-l6.png','kbg-s6.png'
+      'dsg-l6.png','dsg-s6.png'
     ]   
   },
-]
+]/* dsgArr----------------------------------------------- */
 
+
+/* cggArr------------------------------------------------ */
+cggArr = [
+  {
+    century : '1400 ~ 1499',
+    year : '1400',
+    slideImg : 'slide-img1.png',
+    arrow : 'arrow.png',
+    title : '창경궁 창건',
+    yearlyArr : [
+      '<em>1418년</em> <b>태종 신궁으로 수강궁 영건</b>',
+      '<em>1483년</em> <b>수강궁터에 창경궁 영건</b>',
+    ],
+    imgArr : [
+      'cgg-l1.png','cgg-s1.png'
+    ]   
+  },
+  {
+    century : '1500 ~ 1599',
+    year : '1500',
+    slideImg : 'slide-img2.png',
+    arrow : 'arrow.png',
+    title : '창경궁 소실',
+    yearlyArr : [
+      '<em>1592년</em> <b>임진왜란으로 소실</b>',
+    ],
+    imgArr : [
+      'cgg-l2.png','cgg-s2.png'
+    ]   
+  },
+  {
+    century : '1600 ~ 1699',
+    year : '1600',
+    slideImg : 'slide-img3.png',
+    arrow : 'arrow.png',
+    title : '창경궁 중건 그러나 인조반정',
+    yearlyArr : [
+      '<em>1616년</em> <b>명정전, 문정전 등 중건</b>',
+      '<em>1623년</em> <b>인조반정시 저승전 소실</b>',
+      '<em>1624년</em> <b>이괄의 난으로 통명전 등 소실</b>',
+      '<em>1633년</em> <b>통명전,환경전, 함인정 등 중건</b>',
+    ],
+    imgArr : [
+      'cgg-l3.png','cgg-s3.png'
+    ]   
+  },
+  {
+    century : '1700 ~ 1799',
+    year : '1700',
+    slideImg : 'slide-img4.png',
+    arrow : 'arrow.png',
+    title : '창경궁 중건,소실및 복구 ',
+    yearlyArr : [
+      '<em>1777년</em> <b>자경전 건립</b>',
+      '<em>1779년</em> <b>월근문 건립</b>',
+      '<em>1790년</em> <b>통명전 화재</b>',
+    ],
+    imgArr : [
+      'cgg-l4.png','cgg-s4.png'
+    ]   
+  },
+  {
+    century : '1800 ~ 1899',
+    year : '1800',
+    slideImg : 'slide-img5.png',
+    arrow : 'arrow.png',
+    title : '창경궁 화재와 수리',
+    yearlyArr : [
+      '<em>1830년</em> <b>환경전 화재로 함인정 등 소실</b>',
+      '<em>1834년</em> <b>통명전 등 중건</b>',
+      '<em>1857년</em> <b>선인문 등 60여칸 화재</b>',
+      '<em>1877년</em> <b>창경궁 수리</b>',
+    ],
+    imgArr : [
+      'cgg-l5.png','cgg-s5.png'
+    ]   
+  },
+  {
+    century : '1900 ~ 1999',
+    year : '1900',
+    slideImg : 'slide-img6.png',
+    arrow : 'arrow.png',
+    title : '동식물원 조성, 창경원으로 격하',
+    yearlyArr : [
+      '<em>1909년</em> <b>창경궁내 동ㆍ식물원 개설</b>',
+      '<em>1963년</em> <b>사적 지정</b>',
+      '<em>1983년</em> <b>동ㆍ식물 서울대공원에 인계</b>',
+      '<em>1983년</em> <b>창경궁 본래 이름으로 환원</b>',
+    ],
+    imgArr : [
+      'cgg-l6.png','cgg-s6.png'
+    ]   
+  },
+]/* cggArr----------------------------------------------- */
+/* cdgArr------------------------------------------------ */
+cdgArr = [
+  {
+    century : '1400 ~ 1499',
+    year : '1400',
+    slideImg : 'slide-img1.png',
+    arrow : 'arrow.png',
+    title : '창경궁 창건',
+    yearlyArr : [
+      '<em>1405년</em> <b>조선의 이궁으로 건설</b>',
+      '<em>1412년</em> <b>돈화문 건립</b>',
+      '<em>1468년</em> <b>예종이 수강궁 중문에서 즉위</b>',
+      '<em>1494년</em> <b>연산군이 인정문에서 즉위</b>',
+    ],
+    imgArr : [
+      'cdg-l1.png','cdg-s1.png'
+    ]   
+  },
+  {
+    century : '1500 ~ 1599',
+    year : '1500',
+    slideImg : 'slide-img2.png',
+    arrow : 'arrow.png',
+    title : '임진왜란으로 인한 소실',
+    yearlyArr : [
+      '<em>1592년</em> <b>선조의 의주파천</b>',
+      '<em>1592년</em> <b>창덕궁 등 모든 궁궐이 소실됨</b>',
+    ],
+    imgArr : [
+      'cdg-l2.png','cdg-s2.png'
+    ]   
+  },
+  {
+    century : '1600 ~ 1699',
+    year : '1600',
+    slideImg : 'slide-img3.png',
+    arrow : 'arrow.png',
+    title : '창덕궁 중건 그러나 인조반정',
+    yearlyArr : [
+      '<em>1610년</em> <b>창덕궁 중건, 법궁으로 삼음</b>',
+      '<em>1623년</em> <b>창덕궁의 궁궐전각들이 소실됨</b>',
+      '<em>1636년</em> <b>옥류천 주변에 태극정 등 건설</b>',
+      '<em>1647년</em> <b>소실된 건물 중건</b>',
+    ],
+    imgArr : [
+      'cdg-l3.png','cdg-s3.png'
+    ]   
+  },
+  {
+    century : '1700 ~ 1799',
+    year : '1700',
+    slideImg : 'slide-img4.png',
+    arrow : 'arrow.png',
+    title : '제2의 법궁 - 258년간',
+    yearlyArr : [
+      '<em>1704년</em> <b>후원에 대보단 건설</b>',
+      '<em>1749년</em> <b>대보단 중수</b>',
+      '<em>1776년</em> <b>누각을 세움 (주합루,규장각)</b>',
+    ],
+    imgArr : [
+      'cdg-l4.png','cdg-s4.png'
+    ]   
+  },
+  {
+    century : '1800 ~ 1899',
+    year : '1800',
+    slideImg : 'slide-img5.png',
+    arrow : 'arrow.png',
+    title : '재건과 갑신정변',
+    yearlyArr : [
+      '<em>1803년</em> <b>인정전 소실 다음해 재건</b>',
+      '<em>1833년</em> <b>대조전, 희정당 등 소실</b>',
+      '<em>1882년</em> <b>임오군란이 일어남</b>',
+      '<em>1884년</em> <b>관물헌에서 갑신정변이 일어남</b>',
+    ],
+    imgArr : [
+      'cdg-l5.png','cdg-s5.png'
+    ]   
+  },
+  {
+    century : '1900 ~ 1999',
+    year : '1900',
+    slideImg : 'slide-img6.png',
+    arrow : 'arrow.png',
+    title : '자랑스러운 세계유산등록',
+    yearlyArr : [
+      '<em>1917년</em> <b>대 화재로 침전 건물 소실</b>',
+      '<em>1926년</em> <b>순종 창덕궁 대조전에서 승하</b>',
+      '<em>1990년</em> <b>대규모 복원 사업</b>',
+      '<em>1997년</em> <b>유네스코 세계유산으로 등록</b>',
+    ],
+    imgArr : [
+      'cdg-l6.png','cdg-s6.png'
+    ]   
+  },
+]/* cdgArr-------------------------------------------------------------- */
--- sub-cdg/sub-history-cdg.php
+++ sub-cdg/sub-history-cdg.php
@@ -1,4 +1,78 @@
-<section class="sub-content sub-history sub-history-cdg">
-    <h3>창덕궁역사</h3>
+<link rel="stylesheet" href="./css/sub/sub-history.css">
+<script src="./js/sub-history/store.js"></script>
+<script src="./js/sub-history/sub-history.js"></script>
+<section class="sub-content sub-history sub-history-cdg sub-content-center">
+
+<div class="sub-title-wrap sub-title-wrap-center">
+    <h2 class="sub-title">창덕궁 역사</h2>
+    <p class="sub-desc">조선 제일의 법도 경복궁의 역사를 안내드립니다.</p>
   </div><!-- sub-title-wrap -->
+
+  <figure class="bg-deco">
+    <img class="mountain" src="./img/sub/sub-deco/mountain3.png" alt="">
+  </figure>
+
+  <div class="clouds">
+    <img class="cloud right-front" src="./img/sub/sub-deco/right_front.png" alt=""></img>
+    <img class="cloud right-back" src="./img/sub/sub-deco/right_back.png" alt=""></img>
+    <img class="cloud left-front" src="./img/sub/sub-deco/left_front.png" alt=""></img>
+    <img class="cloud left-back" src="./img/sub/sub-deco/left_back.png" alt=""></img>
+  </div>
+
+  <div class="sub-history-content overflow"><!-- 전체 -->
+    <div class="col_width">
+      <ol class="img_container"></ol>
+    </div>
+  </div><!-- content  -->
+  <div class="controls">
+    <div class="indicator">
+      <button class="active">1400</button>
+      <button>1500</button>
+      <button>1600</button>
+      <button>1700</button>
+      <button>1800</button>
+      <button>1900</button>
+    </div>
+    <button class="next"><i class="fa-solid fa-chevron-right"></i></button>
+    <button class="prev"><i class="fa-solid fa-chevron-left"></i></button>
+  </div>
+
+  <script>
+    cdgArr.forEach(function(v) {
+      $(`.sub-history .overflow .img_container`).append(`
+          <li>
+            <figure class="top-img">
+              <img src="./img/sub/sub-history/cdg/${v.slideImg}" alt>
+            </figure>
+            <div class="century-textbox">
+              <p class="century">${v.century}</p>
+              <h3 class="year">${v.year}<span class="point-sans">年</span></h3>
+            </div>
+            <div class="bottom-content">
+              <div class="bottom-textbox">
+                <h4>
+                  <img class="arrow" src="./img/sub/sub-deco/${v.arrow}" alt="">
+                  <em class="history-title">${v.title}</em>
+                </h4>
+                <div class="yearly-textbox"></div>
+              </div>
+                <figure>
+                </figure>
+              </div>
+          </li>
+        `)
+      v.yearlyArr.forEach(function(e) {
+        $(`.img_container >li:last-child .yearly-textbox`).append(`
+            <p class="yearly">
+             ${e}
+            </p>
+            `)
+      }) //yearlyArr
+      v.imgArr.forEach(function(i) {
+        $(`.img_container >li:last-child .bottom-content figure`).append(`
+            <img src="./img/sub/sub-history/cdg/${i}" alt="">
+          `)
+      }) //imgArr
+    }) //historyArr1
+  </script>
 </section>
