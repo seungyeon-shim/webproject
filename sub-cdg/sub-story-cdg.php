24f787119b765f1086d42e650e52be36ce0607cf
심승연 <ssy6240@naver.com>
2023-01-18 20:00:57 +0900

    창경궁, 창덕궁 story / history 이미지 편집

--- sub-cdg/sub-story-cdg.php
+++ sub-cdg/sub-story-cdg.php
@@ -1,3 +1,130 @@
-<section class="sub-content sub-story sub-story-cdg">
-    <h3>창덕궁 이야기</h3>
+<link rel="stylesheet" href="./css/sub/sub-story.css">
+<script src="./js/sub-story/sub-story.js"></script>
+<section class="sub-content sub-story sub-story-cdg sub-content-center">
+
+    <img class="deco back-pattern" src="./img/sub/sub-deco/back-pattern.png" alt="">
+    <img class="deco story-left" src="./img/sub/sub-deco/story-left.png" alt="">
+    <img class="deco story-right" src="./img/sub/sub-deco/story-right.png" alt="">
+
+    <figure class="wave"></figure>
+
+    <div class="sub-title-wrap sub-title-wrap-center">
+        <p class="sub-desc">조선 임금들이 거처했던 궁궐</p>
+        <h2 class="sub-title">창덕궁 이야기</h2>
+    </div>
+
+    <div class="content">
+        <div class="box box1 active" data-n="1">
+            <div class="flip">
+                <div class="face front">
+                    <img class="screen" src="./img/sub/sub-deco/folding-screen1.png" alt="">
+                    <h3 class="ebs">
+                        <img class="brush" src="./img/sub/sub-deco/title-top.png" alt="">
+                        <span>헌종의 사랑이야기</span>
+                    </h3>
+                    <p>
+                        <img class="brush" src="./img/sub/sub-deco/title-bottom.png" alt="">
+                        <span>
+                            왕비보다 후궁 김씨를 더 사랑했던 <br>
+                            헌종이 김씨를 위해 지은 건물
+
+                        </span>
+                    </p>
+                    <img class="main" src="./img/sub/sub-story/cdg/original/1.png" alt="">
+                </div><!-- front -->
+                <div class="face back">
+                    <img src="./img/sub/sub-deco/folding-screen1-back.png" alt="">
+                </div><!-- back -->
+
+            </div><!-- flip -->
+        </div><!-- box1 -->
+
+        <div class="box box2" data-n="2">
+            <div class="flip">
+                <div class="face front">
+                    <img class="screen" src="./img/sub/sub-deco/folding-screen2.png" alt="">
+                    <h3 class="ebs">
+                        <img class="brush" src="./img/sub/sub-deco/title-top.png" alt="">
+                        <span>조선시대 천재 건축가</span>
+                    </h3>
+                    <p>
+                        <img class="brush" src="./img/sub/sub-deco/title-bottom.png" alt="">
+                        <span>
+                            박자청이 산세를 살려서 지어야 한다고 <br>
+                            주장하며 명을 어기고 그냥 사다리꼴을 건축
+
+                        </span>
+                    </p>
+                    <img class="main" src="./img/sub/sub-story/cdg/original/2.png" alt="">
+                </div><!-- front -->
+                <div class="face back">
+                    <img src="./img/sub/sub-deco/folding-screen2-back.png" alt="">
+                </div><!-- back -->
+            </div><!-- flip -->
+        </div><!-- box2 -->
+
+        <div class="box box3" data-n="3">
+            <div class="flip">
+                <div class="face front">
+                    <img class="screen" src="./img/sub/sub-deco/folding-screen3.png" alt="">
+                    <h3 class="ebs">
+                        <img class="brush" src="./img/sub/sub-deco/title-top.png" alt="">
+                        <span>휘령전 앞뜰</span>
+                    </h3>
+                    <p>
+                        <img class="brush" src="./img/sub/sub-deco/title-bottom.png" alt="">
+                        <span>
+                            영조는 1762년 창덕궁 내의 휘령전 <br>
+                            앞뜰에서 사도세자에게 자결을 요구
+                        </span>
+                    </p>
+                    <img class="main" src="./img/sub/sub-story/cdg/original/3.png" alt="">
+                </div><!-- front -->
+                <div class="face back">
+                    <img src="./img/sub/sub-deco/folding-screen3-back.png" alt="">
+                </div><!-- back -->
+            </div><!-- flip -->
+        </div><!-- box3 -->
+        <button class="indicator prev" data-n="-1"><i class="fa-solid fa-angle-left"></i></button>
+        <button class="indicator next" data-n="1"><i class="fa-solid fa-angle-right"></i></button>
+    </div><!-- content -->
+
+    <div class="open-container">
+        <div class="open open1">
+            <h4 class="ebs">헌종의 사랑이야기</h4>
+            <p>
+                1847년 경빈 김씨를 후궁으로 맞이한 후, 김씨를 위해
+                지은 건물이 낙선재, 석복헌, 수강재이다. 24대 임금 헌종의
+                첫 번째 부인은 어린 나이에 숨을 거두었고, 왕비 결정권은
+                대왕대비에게 있었으나, 헌종은 후사를 이어야 한다는
+                명목으로 두번째 부인으로 경빈 김씨를 후궁으로
+                맞이하면서 후궁이 살 집을 새로 짓게 한 것이다.
+
+            </p>
+            <img src="./img/sub/sub-story/cdg/hover/1.png" alt="">
+        </div>
+        <div class="open open2">
+            <h4 class="ebs">조선시대 천재 건축가</h4>
+            <p>
+                세종 1년 태종이 상왕이었을 때, 박자청에게 진선문을
+                통과하여 인정문으로 들어오는 부분을 직사각형으로
+                만들라고 지시함.그러나 박자청이 산세를 살려서 지어야
+                한다고 주장하며 명을 어기고 사다리꼴로 건축. 태종은
+                박자청을 측량을 게을리하였다고 하며, 하옥시키고 건물을
+                부숴버리라고 했는데, 건물은 아직 그대로 남아있다.
+            </p>
+            <img src="./img/sub/sub-story/cdg/hover/2.png" alt="">
+        </div>
+        <div class="open open3">
+            <h4 class="ebs">휘령전 앞뜰</h4>
+            <p>
+                영조는 1762년 창덕궁 내의 휘령전 앞뜰에서 사도세자에게
+                자결을 요구. 이에 사도세자가 땅에 머리를 부딪쳐
+                이마에서 피가 흘러내리고 허리띠로 목을 맸다.
+                그러자 신하들이 자결을 만류하였고, 이에 영조는 화를
+                내며 큰 뒤주를 갖다 놓고는 사도세자를 그 안에 가두었다.
+            </p>
+            <img src="./img/sub/sub-story/cdg/hover/3.png" alt="">
+        </div>
+    </div><!-- open-container -->
 </section>
--- sub-cgg/sub-story-cgg.php
+++ sub-cgg/sub-story-cgg.php
@@ -1,3 +1,138 @@
-<section class="sub-content sub-story sub-story-cgg">
-    <h3>창경궁 이야기</h3>
+<link rel="stylesheet" href="./css/sub/sub-story.css">
+<script src="./js/sub-story/sub-story.js"></script>
+<section class="sub-content sub-story sub-story-cgg sub-content-center">
+
+    <img class="deco back-pattern" src="./img/sub/sub-deco/back-pattern.png" alt="">
+    <img class="deco story-left" src="./img/sub/sub-deco/story-left.png" alt="">
+    <img class="deco story-right" src="./img/sub/sub-deco/story-right.png" alt="">
+
+    <figure class="wave"></figure>
+
+    <div class="sub-title-wrap sub-title-wrap-center">
+        <p class="sub-desc">세 왕후 거처를 위해 지은</p>
+        <h2 class="sub-title">창경궁 이야기</h2>
+    </div>
+
+    <div class="content">
+        <div class="box box1 active" data-n="1">
+            <div class="flip">
+                <div class="face front">
+                    <img class="screen" src="./img/sub/sub-deco/folding-screen1.png" alt="">
+                    <h3 class="ebs">
+                        <img class="brush" src="./img/sub/sub-deco/title-top.png" alt="">
+                        <span>사도세자와 문정전</span>
+                    </h3>
+                    <p>
+                        <img class="brush" src="./img/sub/sub-deco/title-bottom.png" alt="">
+                        <span>
+                            사도세자는 대략 100명을 죽인 조선시대의 <br>
+                            악명높은 연쇄살인마이며, 사도세자에게는 <br>
+                            정신질환증이 있다.
+
+                        </span>
+                    </p>
+                    <img class="main" src="./img/sub/sub-story/cgg/original/1.png" alt="">
+                </div><!-- front -->
+                <div class="face back">
+                    <img src="./img/sub/sub-deco/folding-screen1-back.png" alt="">
+                </div><!-- back -->
+
+            </div><!-- flip -->
+        </div><!-- box1 -->
+
+        <div class="box box2" data-n="2">
+            <div class="flip">
+                <div class="face front">
+                    <img class="screen" src="./img/sub/sub-deco/folding-screen2.png" alt="">
+                    <h3 class="ebs">
+                        <img class="brush" src="./img/sub/sub-deco/title-top.png" alt="">
+                        <span>조선의 온갖 슬픈 역사</span>
+                    </h3>
+                    <p>
+                        <img class="brush" src="./img/sub/sub-deco/title-bottom.png" alt="">
+                        <span>
+                            소현세자빈 강씨의 이야기, 인현왕후를 <br>
+                            주술로 저주해서 중전 복위를 노렸던 희빈 장씨, <br>
+                            감금된 사도세자 사건
+                        </span>
+                    </p>
+                    <img class="main" src="./img/sub/sub-story/cgg/original/2.png" alt="">
+                </div><!-- front -->
+                <div class="face back">
+                    <img src="./img/sub/sub-deco/folding-screen2-back.png" alt="">
+                </div><!-- back -->
+            </div><!-- flip -->
+        </div><!-- box2 -->
+
+        <div class="box box3" data-n="3">
+            <div class="flip">
+                <div class="face front">
+                    <img class="screen" src="./img/sub/sub-deco/folding-screen3.png" alt="">
+                    <h3 class="ebs">
+                        <img class="brush" src="./img/sub/sub-deco/title-top.png" alt="">
+                        <span>동물원이 되던날</span>
+                    </h3>
+                    <p>
+                        <img class="brush" src="./img/sub/sub-deco/title-bottom.png" alt="">
+                        <span>
+                            1909년에 일본인들이 을사늑약과 정미7조약 등으로 <br>
+                            슬픔에 빠진 순종의 마음을 달랜다는 <br>
+                            명목으로 궁궐에 동물을 들임
+                        </span>
+                    </p>
+                    <img class="main" src="./img/sub/sub-story/cgg/original/3.png" alt="">
+                </div><!-- front -->
+                <div class="face back">
+                    <img src="./img/sub/sub-deco/folding-screen3-back.png" alt="">
+                </div><!-- back -->
+            </div><!-- flip -->
+        </div><!-- box3 -->
+        <button class="indicator prev" data-n="-1"><i class="fa-solid fa-angle-left"></i></button>
+        <button class="indicator next" data-n="1"><i class="fa-solid fa-angle-right"></i></button>
+    </div><!-- content -->
+
+    <div class="open-container">
+        <div class="open open1">
+            <h4 class="ebs">사도세자와 문정전</h4>
+            <p>
+                사도세자는 의대증이라는 옷에 대한 심한 집착을
+                가짐. 그래서 사도세자는 옷을 하루에도 수십번
+                갈아입고 옷을 입혀주는 시녀도 살해한 적 있다.
+                사도세자의 행동들로 영조는 정조를 지키기
+                위해서 사도세자를 죽이기로 결심. 결국
+                문명전에서 쌀을 담는 좁고좁은 뒤주에 갇혀
+                8일간 빗물만을 마시다가 끝내 아사.
+
+            </p>
+            <img src="./img/sub/sub-story/cgg/hover/1.png" alt="">
+        </div>
+        <div class="open open2">
+            <h4 class="ebs">조선의 온갖 슬픈 역사</h4>
+            <p>
+                첫 번째 소현세자빈 강씨, 소현세자의 부인 강빈은
+                인조가 누명을 뒤집어 씌운 후, 선인문을 통해 궐 밖으로 퇴출.
+                두 번째 희빈 장씨. 인현왕후가 승하 후 인현왕후를 해하려
+                한 주술행위에 분노한 숙종이 사사의 명을 내려 후에
+                희빈 장씨의 시신이 선인문으로 나감. 세 번째 감금된 사도세자.
+                뒤주에 갇힌 사도세자는 선인문 뒤로 옮겨진 뒤
+                굶주림과 더위에 신음하다가 끝내 숨을 거둠.
+            </p>
+            <img src="./img/sub/sub-story/cgg/hover/2.png" alt="">
+        </div>
+        <div class="open open3">
+            <h4 class="ebs">동물원이 되던날</h4>
+            <p>
+                1909년에 일본인들이 을사늑약과 정미7조약 등으로
+                순종의 마음을 달랜다는 명목으로 궁궐에 동물을 들임.
+                일제는 궁궐 안 전각 일부를 철거, 일본식 정원과 건물을 건설,
+                창경궁을 ‘창경원’으로 개명해 격하. 창경궁은 유원지로 되고,
+                전각을 허물며 많은 훼손을 시행. 왕족의 거주 공간이었던
+                창경원은 개원과 동시에 일반인들에게 개방되면서
+                시민을 위한 공간이 되었다. 또한 동,식물원을 통해
+                희귀한 동물과 식물들을 대중적으로 접할 수 있는
+                낙원의 이미지를 가지게 됨.
+            </p>
+            <img src="./img/sub/sub-story/cgg/hover/3.png" alt="">
+        </div>
+    </div><!-- open-container -->
 </section>
