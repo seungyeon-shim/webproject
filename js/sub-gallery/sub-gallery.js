cee8bc97a5897787d4a4047642248ca55c631908
한주은 <nini0221@naver.com>
2023-01-19 17:35:14 +0900

    갤러리 수정

--- js/sub-gallery/sub-gallery.js
+++ js/sub-gallery/sub-gallery.js
@@ -4,9 +4,9 @@ $(function(){
     var gallery2 = $('.sub-gallery').offset().top
     var gallery3 = $('.sub-location').offset().top
     if(scrY >= gallery2 - winH * 0.6 && scrY < gallery3 - winH * 0.5){
-      //$('.sub-gallery .gallery-bottom, .sub-gallery .gallery-top').addClass('active')
+      $('.sub-gallery .cover').addClass('active')
     }else{
-      //$('.sub-gallery .gallery-bottom, .sub-gallery .gallery-top').removeClass('active')
+      $('.sub-gallery .cover').removeClass('active')
     }
   }
   gallerybg()
--- sub-kbg/sub-gallery-kbg.php
+++ sub-kbg/sub-gallery-kbg.php
@@ -2,93 +2,137 @@
 <script src="./js/sub-gallery/sub-gallery.js"></script>
 <section class="sub-content sub-gallery sub-gallery-kbg sub-content-center">
 
-<!--
-<img class="deco gallery-bg" src="./img/sub/sub-deco/gallery-bg.png" alt="">
-<figure class="bg gallery-figure-bottom">
-  <img class="deco gallery-bottom" src="./img/sub/sub-deco/gallery-top.png" alt="">
-</figure>  figure-bottom -->
-<!--
-<figure class="bg gallery-figure-top">
-  <img class="deco gallery-top" src="./img/sub/sub-deco/gallery-bottom.png" alt="">
-</figure>  figure-top -->
+<img class="gallery-bg" src="./img/sub/sub-deco/gallery-bg.png" alt="">
 
-<div class="sub-title-wrap sub-title-wrap-center">
-  <p class="sub-desc">조선 왕조 제일의 법궁</p>
-  <h2 class="sub-title">경복궁 갤러리</h2>
-</div>
+  <div class="sub-title-wrap sub-title-wrap-center">
+    <p class="sub-desc">조선 왕조 제일의 법궁</p>
+    <h2 class="sub-title">경복궁 갤러리</h2>
+  </div>
 
-<div class="content">
-  <div class="flip-book  book">
+  <div class="content">
+    <div class="flip-book  book">
 
-    <div class="sheet sheet1">
-      <div class="page page-odd page1">
-        <figure class="book-cover-front">
-          <img src="./img/sub/sub-deco/gallery-book-cover-front.png" alt="">
-        </figure>
-        <figure class="book-cover-stamp">
-          <img src="./img/sub/sub-deco/gallery-book-cover-stamp.png" alt="">
-        </figure>
-        <div class="desc">
-          <div class="textbox">
-          <h3 class="ebs">경복궁</h3>
-          <p>태조4년(1395년)에 창건된 조선의 법궁으로
-            대한민국을 대표하는 문화유산
-          </p>
-          </div>
-          <em>조선 왕조 제일의 법궁</em>
-        </div>
+      <div class="sheet sheet1">
+        <div class="page page-odd page1">
+        <div class="star-box">
+          <figure class="star">
+          <img class="star-bg" src="./img/sub/sub-deco/star-bg.png" alt="">
+          </figure><!-- star -->
+          <img class="front-back" src="./img/sub/sub-deco/gallery-book-cover-front.png" alt="">
+        </div><!-- star-box -->
         </div> <!-- page1 -->
-      <div class="page page-even page2">
-          사진1
-      </div> <!-- page2 -->
-    </div><!-- sheet1 -->
+        <div class="page page-even page2">
+          <img class="bg-left" src="./img/sub/sub-deco/gallery-book-bg-left.png" alt="">
+          <div class="inner">
+            <div class="textbox">
+              <div class="title">
+                <h3 class="back">景福宮</h3>
+                <h3 class="front">景福宮</h3>
+                <img src="./img/sub/sub-gallery/kbg/book-sm/1.png" alt="">
+              </div><!-- title -->
+              <p>
+                궁성은 궁을 둘러싸고 있는 높은 담장을 이른다.
+                궁성은 1398년에 동쪽, 남쪽, 서쪽이 조성되었으며, 이때 궁성문인 건춘, 광화, 영추문도 세워졌다.
+                이 보다 뒤에 북쪽 담장이 완성되고 북문인 신무문도 갖추어졌다.
+                궁성은 거의 네모난 형태를 이루고 있고 전면 좌우 끝에는 각루인 동십자각과 서십자각이 있다.
+                경복궁의 궁성은 임진왜란으로 궁궐 건물이 모두 소실되었을 때에도 일반 백성들로부터 궁궐터를 보호하는 역할을 하였다.
+                고종 년간 경복궁 중건 시 궁성이 새 단장을 하게 되었지만 일제강점기 에 앞부분이 헐려나가고 또한국전쟁 때 파손되었다.
+                현재의 궁성은 여러 차례 고쳐 쌓은 것이고, 일부는 본래 위치보다 안으로 들여 쌓은 상태이다.
+              </p>
+            </div><!-- textbox -->
+            <div class="sidetext">
+              <p>
+                궁성宮城 광화문光化門 <br>
+                건춘문建春門
+              </p>
+            </div><!-- sidetext -->
+          </div><!-- inner -->
+        </div> <!-- page2 -->
+      </div><!-- sheet1 -->
 
-    <div class="sheet sheet2">
-      <div class="page page-odd page3">
-          사진2
+      <div class="sheet sheet2">
+        <div class="page page-odd page3">
+          <img class="bg-left" src="./img/sub/sub-deco/gallery-book-bg-right.png" alt="">
+          <div class="inner">
+            <div class="box">
+              <figure>
+                <img src="./img/sub/sub-gallery/kbg/book-sm/2.png" alt="">
+              </figure>
+              <div class="textbox">
+                <p class="left">
+                영추문迎秋門 <br>
+                신무문神武門 동십자각東十字閣 
+                </p>
+                <p class="right">
+                광화문은 경복궁의 정문이다. 광화문은 조선의 법궁에
+                해당하는 궁궐의 정문으로서 다른 궁궐들의 정문과는
+                달리 돌로 높은 석축을 쌓고 그 위에 중층구조의 누각
+                을 세워서 마치 성곽의 성문과 같은 격식으로 장대하게
+                지어졌다. 광화문은 중층으로 된 문루를 받치는 기단석
+                축이 세 개의 홍예문으로 이루어져 있는데,
+                중앙의 홍예문으로는 왕이, 좌우의 홍예문으로는 왕세
+                자와 신하들이 출입하였다. 또한 문루(門樓)에는 종을
+                걸어 두어 시각을 알리는데 사용하였다.
+                광화문은 일제강점기에 조선총독부 건물을 지으면서 건
+                춘문 북쪽으로 옮겼다가 한국전쟁 때 폭격을 맞아 문루
+                가 모두 소실되었다. 그 후 1968년에 경복궁 정문의 위
+                치로 다시 옮겼으나 나무를 사용하지 않고 콘크리트 구
+                조로 복원하는 과정에서 위치 제자리를 찾지 못하였다. 
+                </p>
+              </div><!-- textbox -->
+            </div><!-- box -->
+          </div><!-- inner -->
         </div> <!-- page3 -->
-      <div class="page page-even page4">
+        <div class="page page-even page4">
           사진3
-      </div> <!-- page4 -->
-    </div><!-- sheet2 -->
+        </div> <!-- page4 -->
+      </div><!-- sheet2 -->
 
-    <div class="sheet sheet3">
-      <div class="page page-odd page4">
+      <div class="sheet sheet3">
+        <div class="page page-odd page4">
           사진4
         </div> <!-- page4 -->
-      <div class="page page-even page5">
+        <div class="page page-even page5">
           사진5
-      </div> <!-- page5 -->
-    </div><!-- sheet3 -->
+        </div> <!-- page5 -->
+      </div><!-- sheet3 -->
 
-    <div class="sheet sheet4">
-      <div class="page page-odd page5">
+      <div class="sheet sheet4">
+        <div class="page page-odd page5">
           사진5
         </div> <!-- page4 -->
-      <div class="page page-even page6">
+        <div class="page page-even page6">
           사진6
-      </div> <!-- page5 -->
-    </div><!-- sheet4 -->
+        </div> <!-- page5 -->
+      </div><!-- sheet4 -->
 
-    <div class="sheet sheet5">
-      <div class="page page-odd page6">
+      <div class="sheet sheet5">
+        <div class="page page-odd page6">
           사진6
         </div> <!-- page6 -->
-      <div class="page page-even page7">
-          뒷표지
-      </div> <!-- page7 -->
-    </div><!-- sheet5 -->
+        <div class="page page-even page7">
+          <img src="./img/sub/sub-deco/gallery-book-back.png" alt="">
+        </div> <!-- page7 -->
+      </div><!-- sheet5 -->
 
-  </div><!-- book -->
+    </div><!-- book -->
 
-  <div class="btns">
-    <button class="btn1 active" data-n="1">앞장</button>
-    <button class="btn2" data-n="2">1장</button>
-    <button class="btn3" data-n="3">2장</button>
-    <button class="btn4" data-n="4">3장</button>
-    <button class="btn5" data-n="5">4장</button>
-    <button class="btn6" data-n="6">뒷장</button>
-  </div><!-- btns -->
-</div><!-- content -->
+    <div class="btns">
+      <button class="btn1 active" data-n="1">앞장</button>
+      <button class="btn2" data-n="2">1장</button>
+      <button class="btn3" data-n="3">2장</button>
+      <button class="btn4" data-n="4">3장</button>
+      <button class="btn5" data-n="5">4장</button>
+      <button class="btn6" data-n="6">뒷장</button>
+    </div><!-- btns -->
+  </div><!-- content -->
+
+  <div class="cover cover-top">
+    <img class="bg" src="./img/sub/sub-deco/topbg.png" alt="">
+  </div>
+  <div class="cover cover-bottom">
+    <img class="bg" src="./img/sub/sub-deco/bottombg.png" alt="">
+
+  </div>
 
 </section>
