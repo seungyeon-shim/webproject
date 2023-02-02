fc0213ef5d1dae2fa6b1d80543b1140177791c59
한주은 <nini0221@naver.com>
2023-02-02 12:10:09 +0900

    갤러리 아이폰 오류 최종수정

--- js/sub-gallery/sub-gallery.js
+++ js/sub-gallery/sub-gallery.js
@@ -19,9 +19,14 @@ $(function(){
   var book = new flipBook('.book')
   $('.sub-gallery .btns button').click(function(){
     var n = parseInt($(this).attr('data-n'))
+    var odd = (n*2)-1
     book.flip(n)
     $('.sub-gallery .btns button').removeClass('active')
     $(this).addClass('active')
+    //n =1 page3, n=2 page5, n=3 page7, n=4 page9
+    console.log(odd);
+    $(`.sub-gallery .page-odd .right`).removeClass('actived')
+    $(`.sub-gallery .page${odd} .right`).addClass('actived') 
     if(n>1){
       $('.sub-gallery .content .book .sheet1 .page1 img').css({opacity:'0'})
     }else if(n=1){
